4bcb368b37952bd99fe1898b5db9b2d8063fe445
João Teodoro <joaotdn@gmail.com>
2019-06-10 13:39:01 -0300

    Configuring Company Data Page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import 'Styles/main.scss';
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import VueCurrencyInput from 'vue-currency-input';
 import App from './App';
 import router from 'Router';
 import store from 'Store';
 
 Vue.use(BootstrapVue);
+Vue.use(VueCurrencyInput);
 
 // eslint-disable-next-line no-new
 new Vue({
--- src/pages/CompanyData.vue
+++ src/pages/CompanyData.vue
@@ -2,11 +2,16 @@
   <div>
     <b-row>
       <b-col cols="12">
-        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid amet consectetur doloribus ducimus earum eveniet explicabo fugiat laboriosam libero.</p>
+        <p class="text-muted">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid amet consectetur doloribus ducimus earum eveniet explicabo fugiat laboriosam libero.
+        </p>
       </b-col>
 
       <b-col cols="12">
-        <b-form class="mt-3">
+        <b-form
+          class="mt-3"
+          novalidate
+        >
           <b-form-group
             id="input-group-1"
             label="Company name"
@@ -14,8 +19,8 @@
           >
             <b-form-input
               id="input-name"
-              class="w-75"
               v-model="form.name"
+              class="w-75"
               type="text"
               required
               placeholder="e.g. Your Company Name"
@@ -29,8 +34,13 @@
           >
             <b-form-input
               id="input-spend"
-              class="w-75"
               v-model="form.spend"
+              v-currency="{
+                currency: 'USD',
+                locale: 'en',
+                distractionFree: false
+              }"
+              class="w-75"
               type="text"
               required
               placeholder="e.g. $150,000"
@@ -42,14 +52,33 @@
             label="Company spend ability"
             label-for="input-spend-ability"
           >
-            <b-form-input
-              id="input-spend-ability"
-              class="w-75"
-              v-model="form.spendAbility"
-              type="text"
-              required
-              placeholder="e.g. $150,000 - $330,000"
-            />
+            <b-input-group class="w-75">
+              <b-form-input
+                id="input-spend-ability-max"
+                v-model="form.spendAbilityMax"
+                v-currency="{
+                  currency: 'USD',
+                  locale: 'en',
+                  distractionFree: false
+                }"
+                type="text"
+                required
+                placeholder="Maximum spend (e.g. $330,000)"
+              />
+
+              <b-form-input
+                id="input-spend-ability-min"
+                v-model="form.spendAbilityMin"
+                v-currency="{
+                  currency: 'USD',
+                  locale: 'en',
+                  distractionFree: false
+                }"
+                type="text"
+                required
+                placeholder="Minimum spend (e.g. $150,000)"
+              />
+            </b-input-group>
           </b-form-group>
 
           <b-form-group
@@ -69,8 +98,20 @@
 
           <b-row class="text-right">
             <b-col>
-              <b-button variant="outline-primary" type="reset" class="text-uppercase">Reset</b-button>
-              <b-button variant="primary" type="submit" class="text-uppercase">Submit</b-button>
+              <b-button
+                variant="outline-primary"
+                type="reset"
+                class="text-uppercase"
+              >
+                Reset
+              </b-button>
+              <b-button
+                variant="primary"
+                type="submit"
+                class="text-uppercase"
+              >
+                Submit
+              </b-button>
             </b-col>
           </b-row>
         </b-form>
@@ -80,13 +121,19 @@
 </template>
 
 <script>
+import { CurrencyDirective } from 'vue-currency-input';
 export default {
+  directives: {
+    currency: CurrencyDirective
+  },
+
   data () {
     return {
       form: {
         name: '',
         spend: null,
-        spendAbility: null,
+        spendAbilityMin: null,
+        spendAbilityMax: null,
         notes: ''
       }
     };
